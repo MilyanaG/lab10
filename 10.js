e3d5063b86d9c787e796232b127be5f1f853d9ad
MilyanaG <73168485+MilyanaG@users.noreply.github.com>
2023-12-13 12:28:48 +0300

    Add files via upload

--- 10.js
+++ 10.js
@@ -1,10 +1,11 @@
 var x = 5;
+var y;
 var userAgent = navigator.userAgent;
 if(userAgent.indexOf('OPR') > -1){
-var y = 3*x+5
+   y = 3*x+5;
 } else if (userAgent.indexOf('Chrome') > -1){
-var y = x/2+x/5;;
+   y = x/2+x/5;
 } else if (userAgent.indexOf('Firefox') > -1){
-var y = x+5;
+   y = x+5;
 }
 document.write('Результат: ' + y);
